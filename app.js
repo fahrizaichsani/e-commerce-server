0ef7a96e7d93525a84ff0009fd852a43ab3a56e7
fahrizaichsani <fahrizaichsanirafif@gmail.com>
2024-01-11 15:34:37 +0700

    deploy

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const router = require('./router/router')
+const cors = require('cors')
 
+app.use(cors())
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json())
 app.use(router) 
